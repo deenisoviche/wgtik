7992cabaa1be3e18f0db5bd3f25e81ad000895d5
deenisoviche <121923240+deenisoviche@users.noreply.github.com>
2023-01-04 21:40:20 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,31 +1,35 @@
-#include "CSLL.h"
+#include "MOD8.h"
+
 int main()
 {
-    playlistLagu Q;
-    createPlaylist_1301213072(Q);
-
+    ListLagu D;
     int N = 7;
     infotype Lagu;
     adr P;
 
+    CreateList_1301213072(D);
 
     for (int i = 1; i <= N; i++){
         cout << "Artis : ";
         cin >> Lagu.Artis;
         cout << "Judul : ";
         cin >> Lagu.Judul;
-        cout << endl;
+        cout << "Genre : ";
+        cin >> Lagu.Genre;
+        cout << "Playtime : ";
+        cin >> Lagu.Playtime;
+        cout << endl << endl;
 
         newElement_1301213072(Lagu, P);
-        enqueue_1301213072(Q, P);
+        insertLast_1301213072(D, P);
     }
     cout << "============ Menampilkan List ===========" << endl;
-    showSemuaLagu_1301213072(Q);
+    showSemuaLagu_1301213072(D);
 
-    dequeue_1301213072(Q, P);
+    deleteFirst_1301213072(D, P);
 
     cout << "============ Menampilkan List yang telah dihapus ==============" << endl;
-    showSemuaLagu_1301213072(Q);
+    showSemuaLagu_1301213072(D);
 
     return 0;
 }
